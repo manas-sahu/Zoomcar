888a970f450b3883c5b9309ad38f395bef95fb49
Manas Ranjan SAHU (contractor) <manasranjan.sahu@amadeus.com>
2019-05-13 19:17:22 +0530

    Radio Button

--- src/app/zoom-offers/zoom-offers.component.ts
+++ src/app/zoom-offers/zoom-offers.component.ts
@@ -8,14 +8,35 @@ import {ZoomserviceService} from '../zoomservice.service';
 })
 export class ZoomOffersComponent implements OnInit {
   value: any;
-
+  discount = [
+    60,50, 40, 30
+  ]
+  discountPercentage: any;
   constructor(public zoomService:ZoomserviceService) { }
 
   ngOnInit() {
+    this.getZoomcarData();
+    //this.refreshZoomcar();
+    
+  }
+  refreshZoomcar(){
+    setInterval(() => {
+      this.getZoomcarData();
+      console.log("Refreshed");
+      
+        }, 5000);
+  }
+  getZoomcarData(){
     this.zoomService.getEasternBaganaloreOffers().subscribe(result => {
       this.value = result;
       console.log(result);
     })
+  }
+  radioChangeHandeler(event){
+    this.discountPercentage = event.target.value;
+    console.log(event);
+    console.log(this.discountPercentage);
+    this.getZoomcarData();
     
     
   }
